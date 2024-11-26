97a075d098baf44e71273c2bd20086dabe5936a0
IvanOnohov <ivan.onokh.july@gmail.com>
2024-11-27 06:41:01 +0900

    Добавьте файлы проекта.

---
+++ Data and tiime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+DateOnly now = DateOnly.Parse("2024, 11, 12");
+Console.WriteLine(now);
+var d = DateOnly.Parse("2024, 11, 12");
+Console.WriteLine(d);
+bool s = DateOnly.TryParse("2024, 11, 12", out DateOnly now2);
+Console.WriteLine(s);
+string DateString = "15/10/2024";
+string format = "dd/MM/yyyy";
+DateOnly date2 = DateOnly.ParseExact( DateString, format);
+Console.WriteLine(date2);
+string DateString2 = "15-10-2024";
+string[] formats = {"dd-MM-yyyy","dd/MM/yyyy" };
+Console.WriteLine(DateOnly.TryParseExact(DateString2, formats, out DateOnly now3));
